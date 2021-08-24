31dc5a5db604a92d2bf60e485c2b29466b3907ca
austinxduong <axduong2@gmail.com>
2021-08-24 15:27:42 -0700

    can render a map, but not our ID map. debugging

--- src/components/arcGIS/Map.jsx
+++ src/components/arcGIS/Map.jsx
@@ -1,40 +1,13 @@
 /* eslint-disable max-len */
 import React from 'react';
-import { WebMap, MapView } from '@esri/react-arcgis';
-import * as ReactDOM from 'react-dom';
-import { setDefaultOptions, loadModules } from 'esri-loader';
+import { useWebMap } from 'esri-loader-hooks';
+ 
+function Map() {
+  const [ref] = useWebMap('e691172598f04ea8881cd2a4adaa45ba');
 
-setDefaultOptions({ css: true });
+  return <div style={{ height: 400 }} ref={ref} />;
+}
 
-loadModules(['esri/WebMap, esri/views/MapView'])
-  .then((WebMap) => {
-    const Webmap = new WebMap({
-      portalItem: { // autocasts as new PortalItem()
-        id: '20b24a7f3a7d41768ddd5f4aae911bc9',
-    
-      }  
-      
-    });
-
-    const view = new WebScene({
-      container: 'viewDiv', // Reference to the view div created in step 5
-      Webmap, // Reference to the map object created before the view
-      // zoom: 4, // Sets zoom level based on level of detail (LOD)
-      // center: [15, 65] // Sets center point of view using longitude,latitude
-    });
-
-  });
-
-const MapDisplay = () => {
-  return (
-    <div height="500" width="100%">
-      <WebMap />
-    </div>
-  );
-};
-
-
-
-export default MapDisplay;
+export default Map;
 
 
