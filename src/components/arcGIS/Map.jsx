51bd6ca8d3627fbef8b19d1a9c4347bd19e7f85f
austinxduong <axduong2@gmail.com>
2021-08-24 15:14:08 -0700

    attempt rendering map with arcgis-library

--- src/components/arcGIS/Map.jsx
+++ src/components/arcGIS/Map.jsx
@@ -1,6 +1,29 @@
 /* eslint-disable max-len */
 import React from 'react';
-import { WebMap } from 'react-arcgis';
+import { WebMap, MapView } from '@esri/react-arcgis';
+import * as ReactDOM from 'react-dom';
+import { setDefaultOptions, loadModules } from 'esri-loader';
+
+setDefaultOptions({ css: true });
+
+loadModules(['esri/WebMap, esri/views/MapView'])
+  .then((WebMap) => {
+    const Webmap = new WebMap({
+      portalItem: { // autocasts as new PortalItem()
+        id: '20b24a7f3a7d41768ddd5f4aae911bc9',
+    
+      }  
+      
+    });
+
+    const view = new WebScene({
+      container: 'viewDiv', // Reference to the view div created in step 5
+      Webmap, // Reference to the map object created before the view
+      // zoom: 4, // Sets zoom level based on level of detail (LOD)
+      // center: [15, 65] // Sets center point of view using longitude,latitude
+    });
+
+  });
 
 const MapDisplay = () => {
   return (
@@ -10,4 +33,8 @@ const MapDisplay = () => {
   );
 };
 
+
+
 export default MapDisplay;
+
+
