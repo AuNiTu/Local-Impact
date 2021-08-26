c0bc7ec7c86af2f102a4c0c46e53dfd38e57b216
austinxduong <axduong2@gmail.com>
2021-08-25 22:21:04 -0700

    updated header

--- src/components/constants/Header.jsx
+++ src/components/constants/Header.jsx
@@ -4,6 +4,7 @@ import styles from './styles.css';
 const Header = () => (
   <header className={styles.Header}>
     <h1>Local Impact</h1>
+    <h4>Powered by ESRI, Environmental Systems Research Institute</h4>
   </header>
 );
 
