7d9a378bd69e7f61293f4a7eb20eea9daed14ec3
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-08-31 11:24:36 -0700

    location changer draft

---
+++ src/components/arcGIS/MapLocationChange.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function LocationChange() {
+  return (
+    <>
+      <form>
+        <input />
+        <button>Find Location</button>
+      </form>
+      <button>Get My Location</button>
+      <button>Commit Location to My Account</button>
+    </>
+  );
+}
+
+export default LocationChange;
