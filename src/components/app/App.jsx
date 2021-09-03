6a7c7b7277b9131011b0b4cf81dd856de56f0b82
austinxduong <axduong2@gmail.com>
2021-09-02 19:53:00 -0700

    abouts us page linkedin/github

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -14,6 +14,14 @@ export default function App() {
         <Route path="/" exact component={Home} />
         <Route path="/map" exact component={Content} />
         <Route path="/about" exact component={About} />
+        <Route path="/githubTucker" exact component={() => { window.location = 'https://github.com/Grahf0085'; return null;} }/>
+        <Route path="/githubNick" exact component={() => { window.location = 'https://github.com/NickDayFSD'; return null;} }/>
+        <Route path="/githubAusti" exact component={() => { window.location = 'https://github.com/austinxduong'; return null;} }/>
+        <Route path="/linkedinTucker" exact component={() => { window.location = 'https://www.linkedin.com/in/tuckerhoog/'; return null;} }/>
+        <Route path="/linkedinNick" exact component={() => { window.location = 'https://www.linkedin.com/in/nickdayfsd/'; return null;} }/>
+        <Route path="/linkedinAusti" exact component={() => { window.location = 'https://www.linkedin.com/in/austinxduong/'; return null;} }/>
+
+
         <Redirect to="/" />
       </Switch>
       <Footer />
