551eb02fb24bb1cbf7051215f6a8dc8ed267a77a
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-09-07 19:56:16 -0700

    updated maps to have margin for ease of scrolling

--- src/components/arcGIS/map/NewLoc.jsx
+++ src/components/arcGIS/map/NewLoc.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import styles from '../mapview.css';
+import styles from './map.css';
 
 function FetchingMap() {
   return (
-    <div style={{ height: '60vh', width: '100vw' }} className={styles.fetch}>
+    <div className={styles.loading}>
       {/* <img src="" alt="fetching your location" /> */}
       <h3>👀 Fetching your new location</h3>
     </div>
