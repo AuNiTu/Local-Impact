efcf276fc6c62e82525b4b00366bbc206ee0cb71
austinxduong <axduong2@gmail.com>
2021-09-08 22:08:19 -0700

    changed cloud hosting to AMAZON s3

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -9,7 +9,7 @@ const Video = () => {
           muted 
           loop 
           autoPlay 
-          src="https://res.cloudinary.com/duwtuqr0p/video/upload/v1629916997/pexels-ron-lach-9036211_zstzae.mp4"
+          src="https://localimpactvideo.s3.us-west-2.amazonaws.com/local.mp4"
           style={{
             position:'fixed',
             width: '100%',
